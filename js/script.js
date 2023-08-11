073896403d3e0550b41ae27e4a425105fbe1cf41
SejalChaudhary29 <sejalch2933@gmail.com>
2023-08-11 22:26:36 +0530

    restructure the website outlook and maintaing the js file

--- js/script.js
+++ js/script.js
@@ -1,105 +1,257 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const blogList = document.getElementById("blogList");
-    const addBlogBtn = document.getElementById("addBlogBtn");
-    const addBlogModal = document.getElementById("addBlogModal");
-    const addBlogForm = document.getElementById("addBlogForm");
-    const closeModal = document.querySelector(".close");
-    
-    // Function to display blogs on the home page
-    function displayBlogs() {
-        blogList.innerHTML = ""; // Clear previous content
-        const blogs = JSON.parse(localStorage.getItem("blogs")) || [];
+const addBlogBtn = document.getElementById('add-blog-btn');
+const closeBlogBtn = document.getElementById('close-blog-btn');
+const addBlogSection = document.querySelector('#overlay');
+const blogContainer = document.querySelector('#blog-container');
+const createBlogBtn = document.querySelector('#create-blog-btn');
+const BlogImgUrl = document.querySelector('#post-url');
+const BlogTitle = document.querySelector('#blog-title');
+const BlogDesc = document.querySelector('#blog-desc');
+const BlogContent = document.querySelector('#main-blog');
 
-        blogs.forEach((blog, index) => {
-            const blogItem = document.createElement("div");
-            blogItem.className = "blog-item";
-            blogItem.innerHTML = `
-                <h3>${blog.title}</h3>
-                <p>${blog.description}</p>
-                <button class="read-blog" data-index="${index}">
-                 <a href="blog.html" class= 'read'> Read More</a></button>
-                <button class="delete-blog" data-index="${index}">Delete</button>
-            `;
-            blogList.appendChild(blogItem);
-        });
 
-        // Add event listeners to "Read More" and "Delete" buttons
-        const readButtons = document.querySelectorAll(".read-blog");
-        readButtons.forEach(button => {
-            button.addEventListener("click", showBlog);
-        });
 
-        const deleteButtons = document.querySelectorAll(".delete-blog");
-        deleteButtons.forEach(button => {
-            button.addEventListener("click", deleteBlog);
-        });
-    }
-
-    // Function to open the add blog modal
-    function openAddBlogModal() {
-        addBlogModal.style.display = "block";
-    }
-
-    // Function to close the add blog modal
-    function closeAddBlogModal() {
-        addBlogModal.style.display = "none";
-        addBlogForm.reset(); // Clear form fields after closing
-    }
-
-    // Function to handle form submission for adding a new blog
-    function addBlog(event) {
-        event.preventDefault();
-        const title = document.getElementById("title").value;
-        const poster = document.getElementById("poster").value;
-        const description = document.getElementById("description").value;
-        const content = document.getElementById("content").value;
-        const posterImage = document.getElementById("posterImage").files[0];
-    
-        const newBlog = { title, poster, description, content, posterImage };
-        const blogs = JSON.parse(localStorage.getItem("blogs")) || [];
-        blogs.push(newBlog);
-        localStorage.setItem("blogs", JSON.stringify(blogs));
-        displayBlogs();
-        closeAddBlogModal();
-    }
-
-    
-    // Function to show a single blog post on a separate page
-    function showBlog(event) {
-        const index = event.target.getAttribute("data-index");
-        const blogs = JSON.parse(localStorage.getItem("blogs")) || [];
-
-        const blogPost = blogs[index];
-        const blogPostHTML = `
-        <article class="single-blog">
-        <h2>${blogPost.title}</h2>
-        <img src="${blogPost.posterImage}" alt="Blog Poster">
-        <p><strong>Poster:</strong> ${blogPost.poster}</p>
-        <p>${blogPost.content}</p>
-        <a href="index.html">Back to Home</a>
-    </article>
-        `;
-
-        document.getElementById("blogPost").innerHTML = blogPostHTML;
-    }
-
-    // Function to handle deleting a blog post
-    function deleteBlog(event) {
-        const index = event.target.getAttribute("data-index");
-        const blogs = JSON.parse(localStorage.getItem("blogs")) || [];
-
-        if (confirm("Are you sure you want to delete this blog post?")) {
-            blogs.splice(index, 1);
-            localStorage.setItem("blogs", JSON.stringify(blogs));
-            displayBlogs();
-        }
-    }
-
-    // Event listeners
-    addBlogBtn.addEventListener("click", openAddBlogModal);
-    addBlogForm.addEventListener("submit", addBlog);
-    closeModal.addEventListener("click", closeAddBlogModal);
-
-    // Call the displayBlogs function when the page loads
-    displayBlogs();
+addBlogBtn.addEventListener('click', () => {
+    addBlogSection.classList.add('active-add-blog');
+    document.body.style.overflow = 'hidden';
 });
+
+closeBlogBtn.addEventListener('click', () => {
+    hideAddBlogModal();
+});
+
+function showBlog(title, imageUrl, description, content) {
+    // Hide the blog container and show the individual blog content
+    blogContainer.style.display = 'none';
+
+    const blogContent = document.createElement('section');
+    blogContent.id = 'blog-content';
+    blogContent.classList.add('flex');
+
+    const headerDiv = document.createElement('header');
+    headerDiv.classList.add('flex');
+
+    const leftContentDiv = document.createElement('div');
+    leftContentDiv.classList.add('left-content', 'flex');
+
+    const h2 = document.createElement('h2');
+    h2.textContent = title;
+
+    const h3 = document.createElement('h3');
+    h3.textContent = description;
+
+    leftContentDiv.appendChild(h2);
+    leftContentDiv.appendChild(h3);
+
+    const rightContentDiv = document.createElement('div');
+    rightContentDiv.classList.add('right-content');
+
+    const img = document.createElement('img');
+    img.src = imageUrl;
+    img.alt = 'blog-image';
+
+    rightContentDiv.appendChild(img);
+
+    headerDiv.appendChild(leftContentDiv);
+    headerDiv.appendChild(rightContentDiv);
+
+    const mainContentDiv = document.createElement('div');
+    mainContentDiv.classList.add('main-content');
+
+    const p = document.createElement('p');
+    p.textContent = content;
+
+    mainContentDiv.appendChild(p);
+
+    blogContent.appendChild(headerDiv);
+    blogContent.appendChild(mainContentDiv);
+
+    // Add the blog content to the body
+    document.body.appendChild(blogContent);
+
+    // Show the back button and hide the add blog button
+    document.getElementById('back-btn').style.display = 'grid';
+    document.getElementById('add-blog-btn').style.display = 'none';
+}
+
+function hideBlog() {
+    // Show the blog container and remove the individual blog content
+    blogContainer.style.display = 'flex';
+    document.getElementById('blog-content').remove();
+
+    // Show the add blog button and hide the back button
+    document.getElementById('add-blog-btn').style.display = 'grid';
+    document.getElementById('back-btn').style.display = 'none';
+}
+
+function createBlogPostElement(title, imageUrl, description, content) {
+    const article = document.createElement('article');
+    article.classList.add('blog', 'flex');
+
+    const imageDiv = document.createElement('div');
+    imageDiv.classList.add('image');
+
+    const img = document.createElement('img');
+    img.src = imageUrl;
+    img.alt = 'blog-image';
+
+    imageDiv.appendChild(img);
+
+    const titleDiv = document.createElement('div');
+    titleDiv.classList.add('title');
+
+    const h1 = document.createElement('h1');
+    h1.textContent = title;
+
+    titleDiv.appendChild(h1);
+
+    const descDiv = document.createElement('div');
+    descDiv.classList.add('desc');
+
+    const p = document.createElement('p');
+    p.textContent = description;
+
+    descDiv.appendChild(p);
+
+    const readMoreBtn = document.createElement('button');
+    readMoreBtn.id = 'read-more-btn';
+    readMoreBtn.textContent = 'Read More';
+
+    readMoreBtn.addEventListener('click', () => {
+        window.scrollTo({top: 0})
+        showBlog(title, imageUrl, description, content);
+    });
+
+    article.appendChild(imageDiv);
+    article.appendChild(titleDiv);
+    article.appendChild(descDiv);
+    article.appendChild(readMoreBtn);
+
+    return article;
+}
+
+function addBlogPost(title, imageUrl, description, content) {
+    const blogData = {
+        ImgURL: imageUrl,
+        title,
+        desc: description,
+        content,
+    };
+
+    const blogPostElement = createBlogPostElement(
+        title,
+        imageUrl,
+        description,
+        content
+    );
+
+    blogContainer.appendChild(blogPostElement);
+
+    let existingBlogs = JSON.parse(localStorage.getItem('blogs')) || [];
+    existingBlogs.push(blogData);
+    localStorage.setItem('blogs', JSON.stringify(existingBlogs));
+}
+
+function loadBlogPosts() {
+    let existingBlogs = JSON.parse(localStorage.getItem('blogs')) || [];
+
+    if (existingBlogs.length > 0) {
+        blogContainer.innerHTML = '';
+        existingBlogs.forEach((blogData) => {
+            const blogPostElement = createBlogPostElement(
+                blogData.title,
+                blogData.ImgURL,
+                blogData.desc,
+                blogData.content
+            );
+            blogContainer.appendChild(blogPostElement);
+        });
+    }
+}
+
+function addDemoBlogPosts() {
+    for (const blogData of demoBlog) {
+        const blogPostElement = createBlogPostElement(
+            blogData.title,
+            blogData.ImgURL,
+            blogData.desc,
+            blogData.content
+        );
+        blogContainer.appendChild(blogPostElement);
+    }
+}
+
+function handleAddBlogFormSubmit(event) {
+    event.preventDefault();
+
+    const title = BlogTitle.value;
+    const imageUrl = BlogImgUrl.value;
+    const description = BlogDesc.value;
+    const content = BlogContent.value;
+
+    if (!title || !imageUrl || !description || !content) {
+        alert('Please fill in all fields.');
+        return;
+    }
+
+    addBlogPost(title, imageUrl, description, content);
+
+    BlogTitle.value = '';
+    BlogImgUrl.value = '';
+    BlogDesc.value = '';
+    BlogContent.value = '';
+
+    hideAddBlogModal();
+}
+
+function hideAddBlogModal() {
+    addBlogSection.classList.remove('active-add-blog');
+    document.body.style.overflow = 'auto';
+}
+
+
+
+// Back button functionality
+const backButton = document.getElementById('back-btn');
+backButton.addEventListener('click', hideBlog);
+
+
+// ... (your existing code above)
+
+function displayBlogs() {
+    blogContainer.innerHTML = '';
+    const existingBlogs = JSON.parse(localStorage.getItem('blogs')) || [];
+    existingBlogs.forEach((blog, index) => {
+        const blogDiv = document.createElement('div');
+        blogDiv.className = 'blog';
+        blogDiv.innerHTML = `
+            <img src="${blog.ImgURL}" alt="Blog Image">
+            <h2>${blog.title}</h2>
+            <p>${blog.desc}</p>
+            <button class="read-more-btn" onclick="openBlog(${index})">Read More</button>
+            <button class="delete-blog-btn" onclick="deleteBlog(${index})">Delete</button>
+        `;
+        blogContainer.appendChild(blogDiv);
+    });
+}
+
+// Function to delete a blog post
+function deleteBlog(index) {
+    let existingBlogs = JSON.parse(localStorage.getItem('blogs')) || [];
+
+    if (index >= 0 && index < existingBlogs.length) {
+        existingBlogs.splice(index, 1);
+        localStorage.setItem('blogs', JSON.stringify(existingBlogs));
+        displayBlogs(); // Refresh the displayed blogs
+    }
+}
+
+// ... (your existing code below)
+
+// Add the event listener for deleting blogs
+createBlogBtn.addEventListener('click', handleAddBlogFormSubmit);
+
+window.addEventListener('load', () => {
+    displayBlogs(); // Display the initial set of blogs
+});
+window.addEventListener('load', loadBlogPosts);
+// window.addEventListener('load', addDemoBlogPosts); `
